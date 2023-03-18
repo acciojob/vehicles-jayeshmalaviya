4277a865668792c4228836d2133eb59458fa57d0
jayesh malaviya <jayesh24017@gmail.com>
2023-03-18 11:24:47 +0530

    Initial

--- src/main/java/com/driver/F1.java
+++ src/main/java/com/driver/F1.java
@@ -4,10 +4,12 @@ public class F1 extends Car {
 
     public F1(String name, boolean isManual) {
         //Use arbitrary values for parameters which are not mentioned
+        super(name, 4, 2, 6, isManual, "Sport Car", 2);
     }
 
     public void accelerate(int rate){
         int newSpeed = 0; //set the value of new speed by using currentSpeed and rate
+        newSpeed=getCurrentSpeed()+rate;
         /**
          * speed 0: gear 1
          * speed 1-50: gear 1
@@ -18,13 +20,49 @@ public class F1 extends Car {
          * speed more than 250: gear 6
          */
 
+        if(newSpeed<0)
+        {
+            changeGear(0);
+            stop();
+            changeSpeed(newSpeed, getCurrentDirection());
+        }
         if(newSpeed == 0) {
             //Stop the car, set gear as 1
+            stop();
+            setGears(1);
         }
         //for all other cases, change the gear accordingly
 
-        if(newSpeed > 0) {
-            changeSpeed(newSpeed, getCurrentDirection());
+        else if(newSpeed >=1 && newSpeed<=50) {
+
+            changeGear(1);
+            setCurrentSpeed(newSpeed);
+            move(newSpeed,0);
+        }
+        else if(newSpeed >=51 && newSpeed<=100) {
+            changeGear(2);
+            setCurrentSpeed(newSpeed);
+            move(newSpeed,0);
+        }
+        else if(newSpeed >=101 && newSpeed<=150) {
+            changeGear(3);
+            setCurrentSpeed(newSpeed);
+            move(newSpeed,0);
+        }
+        else if(newSpeed >=151 && newSpeed<=200) {
+            changeGear(4);
+            setCurrentSpeed(newSpeed);
+            move(newSpeed,0);
+        }
+        else if(newSpeed >=201 && newSpeed<=250) {
+            changeGear(5);
+            setCurrentSpeed(newSpeed);
+            move(newSpeed,0);
+        }
+        else if(newSpeed >250) {
+            changeGear(6);
+            setCurrentSpeed(newSpeed);
+            move(newSpeed,0);
         }
     }
 }
